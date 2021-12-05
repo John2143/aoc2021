219b1782b2417ad0244c93be37238be36681f9f6
John Schmidt <john@john2143.com>
2021-12-05 14:01:38 -0500

    d2

--- src/code/p2.rs
+++ src/code/p2.rs
@@ -3,10 +3,48 @@
 use crate::shared;
 use itertools::Itertools;
 
-pub fn read_data() -> impl Iterator<Item = i32> {
-    include_str!("../data/p2.txt");
-    std::iter::once(0)
+pub fn read_data() -> impl Iterator<Item = (&'static str, i32)> {
+    include_str!("../data/p2.txt").lines().map(|x| {
+        let (command, amt) = x.split_once(" ").unwrap();
+        let amt = amt.parse().unwrap();
+        (command, amt)
+    })
 }
 
-pub fn a() {}
-pub fn b() {}
+pub fn a() {
+    let mut depth = 0;
+    let mut distance = 0;
+
+    for (command, amt) in read_data() {
+        match command {
+            "forward" => distance += amt,
+            "down" => depth += amt,
+            "up" => depth -= amt,
+            _ => panic!("invalid command"),
+        }
+    }
+
+    dbg!(depth, distance);
+    println!("depth x distance = {}", depth * distance);
+}
+
+pub fn b() {
+    let mut depth = 0;
+    let mut aim = 0;
+    let mut distance = 0;
+
+    for (command, amt) in read_data() {
+        match command {
+            "forward" => {
+                distance += amt;
+                depth += aim * amt;
+            }
+            "down" => aim += amt,
+            "up" => aim -= amt,
+            _ => panic!("invalid command"),
+        }
+    }
+
+    dbg!(depth, distance);
+    println!("depth x distance = {}", depth * distance);
+}
